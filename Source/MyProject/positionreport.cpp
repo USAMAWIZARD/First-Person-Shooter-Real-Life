5fb9e4580bce2895a75f52b7b6a2c880e444a82d
USAMAWIZARD <usamawizard@gmail.com>
2021-04-17 13:21:40 +0530

    UDP server working

--- Source/MyProject/positionreport.cpp
+++ Source/MyProject/positionreport.cpp
@@ -5,21 +5,29 @@
 #include "Runtime/Networking/Public/Networking.h"
 #include "Runtime/Sockets/Public/Sockets.h"
 #include "Runtime/Sockets/Public/SocketSubsystem.h"
-
+#include "IPAddress.h"
+#include<string.h>
 #include "Gameframework/Actor.h"
-// Sets default values for this component's properties
-FSocket* ListenSocket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("default"), false);
+
+FSocket* ListenSocket;
+FUdpSocketReceiver* UDPReceiver = nullptr;
 
 Upositionreport::Upositionreport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 	// ...
 }
 
+void Upositionreport::RecvData(const FArrayReaderPtr& ArrayReadPrt, const FIPv4Endpoint& EndPt)
+{
+    //FString msg;
+    GLog->Logf(TEXT("Receiver msg"));
+}
 
 // Called when the game starts
+
 void Upositionreport::BeginPlay()
 {
 	Super::BeginPlay();
@@ -29,44 +37,42 @@ void Upositionreport::BeginPlay()
 	FString position = GetOwner()->GetTransform().GetLocation().ToString() ;
 	UE_LOG(LogTemp,Warning,TEXT("class name is %s at %s"),*objname,*position);
 
-    FString TheIP = "172.0.0.1";
+    FString TheIP = "192.168.0.105";
     FString Name = "whogivesa";
-    const int32 ThePort = 4444;
-    FIPv4Address Addr;
+    int ThePort = 6000;
+    FIPv4Address Addr; // = FIPv4Addre` ss(127,0,0,1);
     FIPv4Address::Parse(TheIP, Addr);
 
     //Create Socket
-    FIPv4Endpoint Endpoint(Addr, ThePort);
+    FIPv4Endpoint EndPoint(Addr, ThePort);
+     UE_LOG(LogTemp, Warning, TEXT("csefffffff %s "), *Addr.ToString());
 
     //BUFFER SIZE
     int32 BufferSize = 2 * 1024 * 1024;
 
-     ListenSocket = FUdpSocketBuilder(*Name)
+    ListenSocket = FUdpSocketBuilder(*Name)
         .AsNonBlocking()
         .AsReusable()
-        .BoundToAddress(Addr)
-        .BoundToPort(ThePort)
+        .BoundToEndpoint(EndPoint)
         .Build();
+
+    FTimespan ThreadWaitTime = FTimespan::FromMilliseconds(100);
+
+    UDPReceiver = new FUdpSocketReceiver(ListenSocket, ThreadWaitTime, TEXT("Udp Receiver"));
+
+    UDPReceiver->OnDataReceived().BindUObject(this, &Upositionreport::RecvData);
+
+    UDPReceiver->Start();
+
 }
+
+
 
 
 // Called every frame
 void Upositionreport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-    FString objname = GetOwner()->GetName();
-    FString position = GetOwner()->GetTransform().GetLocation().ToString();
-    UE_LOG(LogTemp, Warning, TEXT("class name is %s at %s"), *objname, *position);
-	// ...
-    uint8* Data = 0;
-    int32 tempBytesCounter;
-
-
-    if (ListenSocket->Recv(Data, sizeof(Data), tempBytesCounter))
-    {
- 
-
-    }
 
 }
+
 
--- Source/MyProject/positionreport.h
+++ Source/MyProject/positionreport.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "IPAddress.h"
+#include "Runtime/Networking/Public/Networking.h"
 #include "positionreport.generated.h"
 
 
@@ -15,14 +17,16 @@ class MYPROJECT_API Upositionreport : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	Upositionreport();
+	
 
 protected:
 	// Called when the game starts
+//	void Recv(const FArrayReaderPtr& ArrayReaderPtr, const FIPv4Endpoint& EndPt);
 	virtual void BeginPlay() override;
 
 public:	
 	// Called every frame
+	virtual void RecvData(const FArrayReaderPtr& ArrayReadPrt, const FIPv4Endpoint& EndPt);
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 		
 };
